0355bd2ec26490495a226dd9e1c7f8cbdfdd8913
Sean <NerdfighterSean@gmail.com>
2012-12-30 02:28:24 -0500

    get amount tipped to a comment/post
    input: thing_id
    output: fullname, tipQTY, amountBTC, amountUSD

---
+++ src/api/gettipped.php
@@ -0,0 +1,46 @@
+<?
+/*
+gettipped.php
+
+Parameters:
+callback : the callback function name ("fff")
+comments : comma separated list of comment ids ("c7ny177,c7hxeas")
+
+Response: Array of JSON objects that correspond to each input comment/post (if it exists)
+fullname : fullname of the comment/post ("t1_c7ny177") (could be "t3_xxxxx" for a post)
+tipQTY : number of contributors ("3")
+amountBTC : total amount of BTC tipped ("0.01")
+amoundUSD : total amount of USD tipped ("0.14") (based on cost to tipper at time of tip)
+
+example: http://bitcointip.net/api/gettipped.php?callback=fff&tipped=c7hxeas,c7ny177
+*/
+
+header('content-type: application/json; charset=utf-8');
+
+//connect to database
+require('init.php');
+
+//get variables
+$tipped = split(",", mysql_real_escape_string($_GET['tipped']));
+$callback = mysql_real_escape_string($_GET['callback']);
+
+$returntipped = array();
+
+//go through the array of tip ids and get the relevent data on each one
+foreach ($tipped as $key => $value)
+{
+$result = mysql_query("SELECT * FROM TEST_TABLE_TIPPED WHERE thing_name LIKE 't%\_$value'", $con);
+while($row = mysql_fetch_array($result))
+		{
+		$fullname = $row['thing_name'];
+		$tipQTY = $row['tips_qty'];
+		$amountBTC = $row['amount_BTC'];
+		$amountUSD = $row['amount_USD'];
+
+
+		array_push($returntipped, array('fullname'=>$fullname, 'tipQTY'=>$tipQTY, 'amountBTC'=>$amountBTC, 'amountUSD'=>$amountUSD));
+		}
+}
+
+echo $callback . '('.json_encode($returntipped).')';
+?>
